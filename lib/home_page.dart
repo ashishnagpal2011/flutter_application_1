dfb8b734e645ff45c735654c438abce5931ef289
an6285 <an6285@gmail.com>
2022-06-05 19:06:16 +0530

    commit 3

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      drawer: Drawer(),
+        body: Center(
+          child: Container(
+            child : Text("Hello World 1")
+          ),
+        ),
+      );
+  }
+}
